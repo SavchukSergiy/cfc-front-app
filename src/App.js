43a6c0ac427ecd72c2a625ae0be3ab66375b03b1
Savchuk Sergiy <savchuksergiy94@gmail.com>
2023-09-04 14:29:37 +0300

    Added pages structure and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './pages/Layout/Layout';
+import Home from './pages/Home/Home';
+import DachaPoNovomy from './pages/DachaPoNovomy/DachaPoNovomy';
+import Catalog from './pages/Catalog/Catalog';
+import Archive from './pages/Archive/Archive';
+import AboutUs from './pages/AboutUs/AboutUs';
+import NoPage from './pages/NoPage/NoPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home/>}/>
+        <Route path="dachaponovomy" element={<DachaPoNovomy/>}/>
+        <Route path="catalog" element={<Catalog/>}/>
+        <Route path="archive" element={<Archive/>}/>
+        <Route path="aboutus" element={<AboutUs/>}/>
+        <Route path="*" element={<NoPage/>}/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
