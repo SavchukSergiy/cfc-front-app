a57e04f3b472b6998a275d846f5d83c653813f67
Savchuk Sergiy <savchuksergiy94@gmail.com>
2025-01-20 18:42:04 +0200

    Hot fix

--- src/components/aboutBlock/aboutblock.js
+++ src/components/aboutBlock/aboutblock.js
@@ -8,13 +8,13 @@ const AboutBlock = () => {
     <>
       <div className='about-us-main'>
         <div className='row'>
-          <div className='col-md-5 about-img-block'>
+          <div className='col-md-5 about-img-block order-1'>
             <div className='aboutimg-border'></div>
             <div className='about-overlay'>
               <img src={AboutImage} alt="About image" class="img-fluid w-100"></img>
             </div>
           </div>
-          <div className='col-md-4 about-text-block'>
+          <div className='col-md-4 about-text-block order-2'>
             <div className='arow-block d-none d-sm-block'>
               <img src={AboutArrow} alt="Arrow image" class="img-fluid"></img>
             </div>
@@ -28,7 +28,7 @@ const AboutBlock = () => {
               </p>
             </div>
           </div>
-          <div className='col-md-3 about-back-image d-none d-sm-block'>
+          <div className='col-md-3 about-back-image d-none d-sm-block order-3'>
             <img src={BgdImage} alt="Background image" class=" back-image-vector img-fluid w-100 "></img>
           </div>
         </div>
