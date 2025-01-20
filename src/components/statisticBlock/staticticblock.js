41bb1cca828a03811a8d37fa3d9d4f28b6bb4f72
Savchuk Sergiy <savchuksergiy94@gmail.com>
2025-01-20 22:35:28 +0200

    worked with statistic block

--- src/components/statisticBlock/staticticblock.js
+++ src/components/statisticBlock/staticticblock.js
@@ -1,29 +1,29 @@
 import './staticticblock.css';
 
-const StatiscitBlock = () => {
+const StatiscitBlock = ({ title1, title2, title3, description1, description2, description3 }) => {
   return (
     <>
-      <div class="container text-center my-5">
+      <div class="container custom-styles text-center my-5">
         <div class="row justify-content-center">
           <div class="col-4 mb-4">
             <div class="position-relative">
               <div class="circle-bg"></div>
-              <h2 class="value">10</h2>
-              <p class="description">Років досвіду</p>
+              <h2 class="value">{title1}</h2>
+              <p class="description">{description1}</p>
             </div>
           </div>
           <div class="col-4 mb-4">
             <div class="position-relative">
               <div class="circle-bg"></div>
-              <h2 class="value">50+</h2>
-              <p class="description">Років гарантії</p>
+              <h2 class="value">{title2}</h2>
+              <p class="description">{description2}</p>
             </div>
           </div>
           <div class="col-4 mb-4">
             <div class="position-relative">
               <div class="circle-bg"></div>
-              <h2 class="value">100%</h2>
-              <p class="description">Фіксована ціна</p>
+              <h2 class="value">{title3}</h2>
+              <p class="description">{description3}</p>
             </div>
           </div>
         </div>
